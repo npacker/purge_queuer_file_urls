736b649d56700e13ff51b5e663731f2290b8624c
Nigel Packer <packern@wwu.edu>
2025-03-28 13:46:05 +0000

    Add comment documentation to scheme filter decorator

--- src/SchemeFilteredUrlExpressionFactory.php
+++ src/SchemeFilteredUrlExpressionFactory.php
@@ -6,8 +6,22 @@ use Drupal\File\FileInterface;
 use Drupal\image\ImageStyleInterface;
 use Drupal\purge_queuer_file_urls\Service\StreamWrapperFilterServiceInterface;
 
+/**
+ * Decorates UrlExpressionFactory objects.
+ *
+ * Filters out files and image style paths via the stream wrapper filter
+ * service..
+ */
 class SchemeFilteredUrlExpressionFactory implements UrlExpressionFactoryInterface {
 
+  /**
+   * Constructs a new SchemeFilteredUrlExpressionFactory object.
+   *
+   * @param \Drupal\purge_queuer_file_urls\UrlExpressionFactoryInterface $inner
+   *   The inner URL expression factory.
+   * @param \Drupal\purge_queuer_file_urls\Service\StreamWrapperFilterServiceInterface $streamWrapperFilterService
+   *   The stream wrapper filter service.
+   */
   public function __construct(
     protected readonly UrlExpressionFactoryInterface $inner,
     protected readonly StreamWrapperFilterServiceInterface $streamWrapperFilterService,
@@ -17,8 +31,7 @@ class SchemeFilteredUrlExpressionFactory implements UrlExpressionFactoryInterfac
    * {@inheritdoc}
    */
   public function generateFromFile(FileInterface $file): \Generator {
-    $filtered_files = $this->streamWrapperFilterService->filterFiles([$file]);
-    if ($filtered_files) {
+    if ($this->streamWrapperFilterService->filterFiles([$file])) {
       yield from $this->inner->generateFromFile(array_pop($filtered_files));
     }
   }
