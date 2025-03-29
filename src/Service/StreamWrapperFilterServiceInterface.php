282012b6da477549bc124d9d507047d8d903a83f
Nigel Packer <packern@wwu.edu>
2025-03-29 02:55:26 +0000

    Document StreamWrapperFilterServiceInterface

--- src/Service/StreamWrapperFilterServiceInterface.php
+++ src/Service/StreamWrapperFilterServiceInterface.php
@@ -2,10 +2,31 @@
 
 namespace Drupal\purge_queuer_file_urls\Service;
 
+/**
+ * Defines an interface for services to filter files and URIs.
+ */
 interface StreamWrapperFilterServiceInterface {
 
+  /**
+   * Filter an array of files.
+   *
+   * @param \Drupal\File\FileInterface[] $files
+   *   An array of files.
+   *
+   * @return \Drupal\File\FileInterface[]
+   *   The filtered array of files.
+   */
   public function filterFiles(array $files = []): array;
 
+  /**
+   * Filter an array of URIs.
+   *
+   * @param \Drupal\File\string[] $files
+   *   An array of URIs.
+   *
+   * @return \Drupal\File\string[]
+   *   The filtered array of URIs.
+   */
   public function filterUris(array $uris = []): array;
 
 }
