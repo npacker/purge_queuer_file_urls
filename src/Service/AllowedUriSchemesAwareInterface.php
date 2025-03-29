462beefad356dd35178dfdbf490073b1a5bf6c71
Nigel Packer <packern@wwu.edu>
2025-03-29 02:49:53 +0000

    Document AllowedUriSchemesAwareInterface

--- src/Service/AllowedUriSchemesAwareInterface.php
+++ src/Service/AllowedUriSchemesAwareInterface.php
@@ -2,8 +2,20 @@
 
 namespace Drupal\purge_queuer_file_urls\Service;
 
+/**
+ * Defines an interface for file scheme setting injection.
+ *
+ * This gives classes the ability to interface with the allowed uri schemes
+ * configurator.
+ */
 interface AllowedUriSchemesAwareInterface {
 
+  /**
+   * Set the allowed URI schemes.
+   *
+   * @param string[] $allowed_uri_schemes
+   *   An array of the allowed URI schemes.
+   */
   public function setAllowedUriSchemes(array $allowed_uri_schemes): void;
 
 }
