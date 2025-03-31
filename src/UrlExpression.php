8340f47fdda25de43d6185f8adb109dbb3e5e324
Nigel Packer <packern@wwu.edu>
2025-03-31 11:34:05 +0000

    Use constructor property promotion for UrlExpression

--- src/UrlExpression.php
+++ src/UrlExpression.php
@@ -8,31 +8,17 @@ use Drupal\purge\Plugin\Purge\Invalidation\InvalidationsServiceInterface;
 class UrlExpression implements UrlExpressionInterface {
 
   /**
-   * The invalidation type plugin.
-   *
-   * @var string
-   */
-  protected $invalidationType;
-
-  /**
-   * The invalidation expression.
-   *
-   * @var \Drupal\Core\Url
-   */
-  protected $expression;
-
-  /**
    * Creates a new UrlExpression object.
    *
-   * @param string $invalidation_type
+   * @param string $invalidationType
    *   The invalidation plugin type associated with this expression.
    * @param \Drupal\Core\Url $expression
    *   The invalidation expression.
    */
-  public function __construct(string $invalidation_type, Url $expression) {
-    $this->invalidationType = $invalidation_type;
-    $this->expression = $expression;
-  }
+  public function __construct(
+    protected readonly string $invalidationType,
+    protected readonly Url $expression
+  ) {}
 
   /**
    * {@inheritdoc}
