11c8b0ca4b3938821a2cd8f428a4ff372500e7f9
Nigel Packer <npacker@users.noreply.github.com>
2025-03-31 15:07:48 -0700

    Add doc block comment to UrlBase

--- src/Plugin/Purge/Invalidation/UrlBase.php
+++ src/Plugin/Purge/Invalidation/UrlBase.php
@@ -8,6 +8,13 @@ use Drupal\purge\Plugin\Purge\Invalidation\Exception\InvalidExpressionException;
 use Drupal\purge\Plugin\Purge\Invalidation\InvalidationBase;
 use Drupal\purge\Plugin\Purge\Invalidation\InvalidationInterface;
 
+/**
+ * Base class for URL-based cache invalidations.
+ *
+ * This abstract class provides a foundation for implementing cache
+ * invalidation strategies that are based on URLs. It includes methods to
+ * validate and represent URLs as expressions.
+ */
 abstract class UrlBase extends InvalidationBase implements InvalidationInterface {
 
   /**
